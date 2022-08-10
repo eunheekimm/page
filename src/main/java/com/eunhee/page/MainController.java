7de68108ffd3dee82a875813dbf733ca95072156
eunheekimm <dmsgml_08@naver.com>
2022-08-11 01:52:42 +0900

    jsp로 기본 페이지 틀 설정

--- src/main/java/com/eunhee/page/MainController.java
+++ src/main/java/com/eunhee/page/MainController.java
@@ -3,12 +3,19 @@ package com.eunhee.page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/introduce", method = RequestMethod.GET)
+    public ModelAndView introducePageWithUrl() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("githubUrl","https://github.com/eunheekimm");
+        modelAndView.addObject("instagramUrl","https://www.instagram.com/carpe_d.iem/");
+        modelAndView.addObject("naverBlogUrl","https://blog.naver.com/dmsgml_08/222701199180");
+        
+        return modelAndView;
     }
+
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,8 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
 <html>
 <head>
-    <title>Spring Boot JSP</title>
+    <title>Kim EunHee's Page</title>
 </head>
 <body>
-<h2> Hello Spring Boot </h2>
+<h2> Introducing Me With My Pages</h2>
+<a href="${githubUrl}">
+    <img class="img-github" src="/images/github_logo.png"/>
+</a>
+<a href="${instagramUrl}">
+    <img class="img-instagram" src="/images/instagram_logo.png"/>
+</a>
+<a href="${naverBlogUrl}">
+    <img class="img-naver-blog" src="/images/naver_blog_logo.png"/>
+</a>
+
+
 </body>
 </html>

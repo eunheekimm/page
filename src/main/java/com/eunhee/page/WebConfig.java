6301953d07c6cd2462e392dbf24b148846783e81
eunheekimm <107455364+eunheekimm@users.noreply.github.com>
2022-08-10 22:30:47 +0900

    start

---
+++ src/main/java/com/eunhee/page/WebConfig.java
@@ -0,0 +1,10 @@
+package com.eunhee.page;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Spring Boot JSP</title>
+</head>
+<body>
+<h2> Hello Spring Boot </h2>
+</body>
+</html>
